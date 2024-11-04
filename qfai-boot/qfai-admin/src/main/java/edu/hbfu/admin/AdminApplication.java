085e507ecbd3c1fbf6d2c7c32e07d1e3ca488355
Poolbee <Dr8462643@outlook.com>
2024-11-04 16:05:12 +0800

    实现项目的后端代码的初始化创建

---
+++ qfai-boot/qfai-admin/src/main/java/edu/hbfu/admin/AdminApplication.java
@@ -0,0 +1,12 @@
+package edu.hbfu.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminApplication {
+    public static void main(String[] args) {
+        //都是一样的，启动
+        SpringApplication.run(AdminApplication.class,args);
+    }
+}
